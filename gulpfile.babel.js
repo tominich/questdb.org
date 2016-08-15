ad39d6afc7610a1dab3549aef0b04dc57eae88cf
Vlad Ilyushchenko <bluestreak@gmail.com>
2016-08-15 15:17:07 +0100

    landing page

---
+++ gulpfile.babel.js
@@ -0,0 +1,179 @@
+/*******************************************************************************
+ *    ___                  _   ____  ____
+ *   / _ \ _   _  ___  ___| |_|  _ \| __ )
+ *  | | | | | | |/ _ \/ __| __| | | |  _ \
+ *  | |_| | |_| |  __/\__ \ |_| |_| | |_) |
+ *   \__\_\\__,_|\___||___/\__|____/|____/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (C) 2016 Appsicle
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+
+// generated on 2016-03-23 using generator-webapp 2.0.0
+import gulp from "gulp";
+import gulpLoadPlugins from "gulp-load-plugins";
+import browserSync from "browser-sync";
+import del from "del";
+import {stream as wiredep} from "wiredep";
+import path from "path";
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+const dist = 'public';
+
+gulp.task('scripts', () => {
+    return gulp.src(['app/scripts/**/*.js', 'app/vendor/*.js'])
+        .pipe($.plumber())
+        .pipe($.sourcemaps.init())
+        .pipe($.babel())
+        .pipe($.sourcemaps.write('.'))
+        .pipe(gulp.dest('.tmp/scripts'))
+        .pipe(reload({stream: true}));
+});
+
+function lint(files, options) {
+    return () => {
+        return gulp.src(files)
+            .pipe(reload({stream: true, once: true}))
+            .pipe($.eslint(options))
+            .pipe($.eslint.format())
+            .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+    };
+}
+const testLintOptions = {
+    env: {
+        mocha: true
+    }
+};
+
+gulp.task('lint', lint('app/scripts/**/*.js'));
+gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
+
+gulp.task('html', ['scripts'], () => {
+    return gulp.src('app/*.html')
+        .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
+        .pipe($.if('*.js', $.uglify()))
+        .pipe($.if('*.css', $.cssnano()))
+        .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+        .pipe(gulp.dest(dist));
+});
+
+gulp.task('images', () => {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            interlaced: true,
+            // don't remove IDs from SVGs, they are often used
+            // as hooks for embedding and styling
+            svgoPlugins: [{cleanupIDs: false}]
+        })))
+        .pipe(gulp.dest(path.join(dist, 'images')));
+});
+
+gulp.task('fonts', () => {
+    return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {
+        })
+        .concat('app/fonts/**/*'))
+        // .pipe(gulp.dest('.tmp/fonts'))
+        .pipe(gulp.dest(path.join(dist, 'fonts')));
+});
+
+gulp.task('extras', () => {
+    return gulp.src([
+        'app/*.*',
+        '!app/*.html'
+    ], {
+        dot: true
+    }).pipe(gulp.dest(dist));
+});
+
+gulp.task('clean', del.bind(null, ['.tmp', dist], {force: true}));
+
+gulp.task('serve', ['scripts', 'fonts'], () => {
+    browserSync({
+        notify: false,
+        port: 9000,
+        server: {
+            baseDir: ['.tmp', 'app'],
+            routes: {
+                '/bower_components': 'bower_components'
+            }
+        }
+    });
+
+    gulp.watch([
+        'app/*.html',
+        'app/images/**/*',
+        '.tmp/fonts/**/*'
+    ]).on('change', reload);
+
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('app/fonts/**/*', ['fonts']);
+    gulp.watch('bower.json', ['wiredep', 'fonts']);
+});
+
+gulp.task('serve:dist', () => {
+    browserSync({
+        notify: false,
+        port: 9000,
+        server: {
+            baseDir: [dist]
+        }
+    });
+});
+
+gulp.task('serve:test', ['scripts'], () => {
+    browserSync({
+        notify: false,
+        port: 9000,
+        ui: false,
+        server: {
+            baseDir: 'test',
+            routes: {
+                '/scripts': '.tmp/scripts',
+                '/bower_components': 'bower_components'
+            }
+        }
+    });
+
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('test/spec/**/*.js').on('change', reload);
+    gulp.watch('test/spec/**/*.js', ['lint:test']);
+});
+
+// inject bower components
+gulp.task('wiredep', () => {
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            exclude: ['bootstrap-sass'],
+            ignorePath: /^(\.\.\/)*\.\./
+        }))
+        .pipe(gulp.dest('app'));
+});
+
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+    return gulp.src(path.join(dist, '**/*')).pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', ['clean'], () => {
+    gulp.start('build');
+});
